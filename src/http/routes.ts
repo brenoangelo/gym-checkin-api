ac9244fad9650691041762141767a740f52ffb61
Breno Angelo <brenoangeloal@gmail.com>
2026-06-28 22:01:05 -0300

    feat: implementando rota de autenticacao do usuario

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import type { FastifyInstance } from 'fastify'
+import { authenticate } from '@/http/controllers/authenticate'
 import { register } from '@/http/controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
