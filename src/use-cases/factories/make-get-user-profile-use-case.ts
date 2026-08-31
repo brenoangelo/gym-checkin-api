5f052dae2ccb281b0e291e175c6056a1d6674eb9
Breno Angelo <brenoangeloal@gmail.com>
2026-08-31 13:29:27 -0300

    feat: criando repositorios, e criando restante dos factories

---
+++ src/use-cases/factories/make-get-user-profile-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { GetUserProfileUseCase } from '@/use-cases/get-user-profile'
+
+export function makeGetProfileUseCase() {
+  const prismaUsersRepository = new PrismaUsersRepository()
+  const useCase = new GetUserProfileUseCase(prismaUsersRepository)
+
+  return useCase
+}
